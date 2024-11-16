8c8cc7a9bc17bc3293a15d9cd348cb9c33061f22
Crypt0zauruS <101745417+Crypt0zauruS@users.noreply.github.com>
2024-11-16 16:12:23 -0500

    added Money Game main menu

--- src/pages/money-game/MoneyGame.js
+++ src/pages/money-game/MoneyGame.js
@@ -1,22 +1,41 @@
-import { useState } from 'react';
-import PlayScreen from './pages/play-screen/PlayScreen';
-import IntroScreen from './pages/intro/IntroScreen'
+// MoneyGame.js
+import React, { useState } from "react";
+import MainMenu from "./pages/menu/MainMenu";
+import Game from "./pages/game/Game";
+import IntroScreen from "./pages/intro/IntroScreen";
+import PlayScreen from "./pages/play-screen/PlayScreen";
 
-function MoneyGame() {
-    const [gameState, setGameState] = useState('PlayScreen');
+const MoneyGame = () => {
+  const [currentScreen, setCurrentScreen] = useState("menu");
+  const [showCredits, setShowCredits] = useState(false);
 
-    let content;
-    if (gameState === 'PlayScreen') {
-        content = <PlayScreen setGameState={setGameState} />;
-    } else if (gameState === 'IntroScreen') {
-        content = <IntroScreen setGameState={setGameState} />;
+  const handlePlayClick = () => {
+    setCurrentScreen("intro");
+  };
+
+  const handleCreditsClick = () => {
+    setShowCredits(true);
+  };
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case "menu":
+        return (
+          <MainMenu
+            onPlayClick={handlePlayClick}
+            onCreditsClick={handleCreditsClick}
+          />
+        );
+      case "intro":
+        return <IntroScreen onComplete={() => setCurrentScreen("play")} />;
+      case "play":
+        return <PlayScreen />;
+      default:
+        return <MainMenu />;
     }
-    
-    return (
-        <div>
-            {content}
-        </div>
-    );
-}
+  };
+
+  return <div className="money-game">{renderScreen()}</div>;
+};
 
 export default MoneyGame;
